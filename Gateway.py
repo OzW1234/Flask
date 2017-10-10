ffde6ffae05585819fff82026ccb3d87fc7d3da1
OzW <ozvvizer@gmail.com>
2017-10-10 18:17:40 +0300

    Gatrnet-Flask

--- Gateway.py
+++ Gateway.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,4 +10,4 @@ def Hello():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host = '0.0.0.0' , port = int(os.environ.get("PORT", 5000)))

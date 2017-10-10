33a2dc925cbb0e0ba50c2498047dd2ec133a8a2e
OzW <ozvvizer@gmail.com>
2017-10-10 21:04:56 +0300

    Gatrnet-Flask

--- Gateway.py
+++ Gateway.py
@@ -1,13 +1,26 @@
 import os
 from flask import Flask
+from flask import Flask
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def Hello():
-  return "Hello World!"
+json=[]
+@app.route('/webhook', methods=['POST'])
+def addOne():
+    new_message = request.get_json()
+    json.append(new_message)
+    return jsonify({'json' : json})
 
-
+@app.route('/webhook', methods=['PUT'])
+def editOne(name):
+    new_json = request.get_json()
+    for i,q in enumerate(json):
+      if q['name'] == name:
+        json[i] = new_json
+    qs = request.get_json()
+    return jsonify({'json' : json})
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0' , port = int(os.environ.get("PORT", 5000)))
